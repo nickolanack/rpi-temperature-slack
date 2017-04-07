47c13de3bd046d9d03fc52713ccf09b8733376d4
nickolanack <nickblackwell82@gmail.com>
2017-04-07 13:50:30 -0700

    display results as a table

--- index.js
+++ index.js
@@ -5,12 +5,35 @@ var Slack = require('slack-node');
 slack = new Slack();
 slack.setWebhook(require('./slack.json').url);
 
-
+var temps={}
+var counter=0;
 
 var Temperature = require('node-rpio-temperature');
 (new Temperature()).on('update', function(temp) {
+
+	temps[temp.device]=temp;
+
+	if(counter%10==0){
+		slack.webhook({
+			text: + 'Devices: '+ Object.keys(temps).map(function(name){
+				return name.substring(name.length-6);
+			}).join(', ');
+		}, function(err, response) {
+
+			if (err) {
+				console.error(err);
+			}
+
+		});
+		counter++;
+	}
+
+	
+
 	slack.webhook({
-		text:'```'+temp.value + '``` ' + temp.units + '  -  '+ temp.device.substring(temp.device.length-4)
+		text: Object.keys(temps).map(function(name){
+			return temps[name].value;
+		}).join(', ')+ temp.units
 	}, function(err, response) {
 
 		if (err) {
